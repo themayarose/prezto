1b4ea125c6774f81c82649bc8e6e649a8c219db6
Ugo Pozo <ugopozo@camara.sp.gov.br>
2016-04-26 19:12:38 -0300

    Fixed locale handling of 'la' alias.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -91,7 +91,7 @@ else
 fi
 
 alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
+alias ll='LC_ALL="C.UTF-8" ls -lh'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
 alias la='ll -A'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
