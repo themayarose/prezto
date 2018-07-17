8f8ab4816aae09d486ec56c978ab9245d7bde6e1
Ugo Pozo <ugopozo@camara.sp.gov.br>
2018-07-17 13:04:55 -0300

    Fixed tmux for computers with no battery, show short hostname if not under ssh.

--- battery.sh
+++ battery.sh
@@ -4,7 +4,7 @@ to_try=("battery" "BAT0")
 
 for bat in $to_try; do
     if [[ -a "/sys/class/power_supply/$bat/capacity" ]]; then
-        cat "/sys/class/power_supply/$bat/capacity"
+        echo " ♥ $(cat /sys/class/power_supply/$bat/capacity)%  "
         break
     fi
 done
