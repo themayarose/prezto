e0aaaa1607bbb429deefbf82f510f6347d0950b4
Ugo Pozo <ugopozo@ugopozo.io>
2018-07-17 02:35:34 -0300

    Fixed battery display for non-Ubuntu.

---
+++ battery.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+to_try=("battery" "BAT0")
+
+for bat in $to_try; do
+    if [[ -a "/sys/class/power_supply/$bat/capacity" ]]; then
+        cat "/sys/class/power_supply/$bat/capacity"
+        break
+    fi
+done
